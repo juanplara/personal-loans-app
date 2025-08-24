bbf998e2da6770a946ca16f45383ec86507e274e
Juan Pablo Lara Angulo <juanpablolarangu@gmail.com>
2025-08-24 11:27:21 -0500

    refactor: se agrega la resticcion al middleware para que el usuario logueado no vuelva a entrar al login

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,14 +3,21 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const token = request.cookies.get('token')?.value;
+    const { pathname } = request.nextUrl;
 
-    if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
+    // 🔹 Proteger Dashboard
+    if (!token && pathname.startsWith('/dashboard')) {
         return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // 🔹 Evitar que un usuario autenticado entre al login
+    if (token && pathname.startsWith('/login')) {
+        return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/dashboard/:path*'],
+    matcher: ['/dashboard/:path*', '/login'],
 };
